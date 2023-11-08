8bb313f8ecbb5feed8e3f631017433642cefe3c8
Charles McNulty <charles.mcnulty@tasconline.com>
2023-11-08 09:24:21 -0600

    matches ssi-report

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const earningsFromXml = await parse('social-security-statement-2023.xml');
 console.log( calc(earningsFromXml) );
 
 const start = 2023;
-const years = 21;
+const years = 67-(start-1977);
 const rate = 0.02;
 
 const missing = (earnings, start, years, rate) =>
@@ -15,7 +15,7 @@ const missing = (earnings, start, years, rate) =>
     ({...earnings, ...compound(Object.values(earnings).at(-1),0,years,rate).result
         .slice(1) // slice off the first result, which is a duplicate of last earnings
         // use reduce to return object which continues the year series
-        .reduce((acc, cur, i) => ((acc[start+i] = Math.round(cur)) && acc),{})
+        .reduce((acc, cur, i) => (acc[start+i] = Math.round(cur), acc),{})
     });
 
 
